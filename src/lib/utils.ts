7d6a9fc356a7b884618aba1fcd77e039268c577b
Fabio Alves Feitoza <falvesf@gmail.com>
2026-08-03 12:51:20 -0300

    Permite caminhos relativos na funcao getSafeUrl

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -3,8 +3,14 @@ export function getSafeUrl(url?: string | null): string | undefined {
   if (url.startsWith('http') || url.startsWith('data:')) return url;
   
   let safeUrl = url.replace(/\\/g, '/');
+  
+  // Se for um caminho absoluto da raiz (ex: /img/...), remove a barra inicial para concatenar
   if (safeUrl.startsWith('/')) {
     safeUrl = import.meta.env.BASE_URL + safeUrl.substring(1);
+  } else {
+    // Se for um caminho relativo puro (ex: img/...), concatena diretamente
+    safeUrl = import.meta.env.BASE_URL + safeUrl;
   }
+  
   return safeUrl;
 }
